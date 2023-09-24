439510ca2e5bb9d296c913c8838b0e00b826c006
bakai-zhumadylov <developer.bakai@gmail.com>
2023-09-24 22:28:09 +0600

    Install laravel and docker

---
+++ api/docker/production/php-bin/swoole/start-container.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+set -e
+
+container_mode=${CONTAINER_MODE:-app}
+octane_server=${OCTANE_SERVER:-swoole}
+echo "Container mode: $container_mode"
+
+php() {
+  su octane -c "php $*"
+}
+
+initialStuff() {
+    php artisan optimize:clear; \
+    php artisan package:discover --ansi; \
+    php artisan event:cache; \
+    php artisan config:cache; \
+    php artisan route:cache;
+}
+
+if [ "$1" != "" ]; then
+    exec "$@"
+elif [ ${container_mode} = "app" ]; then
+    echo "Octane server: $octane_server"
+    initialStuff
+    if [ ${octane_server}  = "swoole" ]; then
+        exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.app.conf
+    elif [ ${octane_server}  = "roadrunner" ]; then
+        exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.app.roadrunner.conf
+    else
+        echo "Invalid Octane server supplied."
+        exit 1
+    fi
+elif [ ${container_mode} = "horizon" ]; then
+    initialStuff
+    exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.horizon.conf
+elif [ ${container_mode} = "scheduler" ]; then
+    initialStuff
+    exec supercronic /etc/supercronic/laravel
+else
+    echo "Container mode mismatched."
+    exit 1
+fi
+
+
+HOST_DOMAIN="host.docker.internal"
+if ! ping -q -c1 $HOST_DOMAIN > /dev/null 2>&1
+then
+  HOST_IP=$(ip route | awk 'NR==1 {print $3}')
+  # shellcheck disable=SC2039
+  echo -e "$HOST_IP\t$HOST_DOMAIN" >> /etc/hosts
+fi
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- php-fpm "$@"
+fi
+
+exec "$@"
